cd0a82d30dfd70f482ac99a837a50f95ee94913a
Iryna <Yurchak.Iryna2017@gmail.com>
2025-10-03 14:12:48 +0300

    New solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,12 +11,15 @@ function centerSpider() {
   const left = (wall.clientWidth - spider.clientWidth) / 2;
   const height = (wall.clientHeight - spider.clientHeight) / 2;
 
+  spider.style.position = 'absolute';
   spider.style.left = `${left}px`;
   spider.style.top = `${height}px`;
 }
 
-window.addEventListener('load', () => {
-  if (getComputedStyle(spider).position === 'absolute') {
-    centerSpider();
-  }
-});
+window.addEventListener('load', centerSpider);
+
+window.addEventListener('resize', centerSpider);
+
+if (spider && spider.complete) {
+  centerSpider();
+}
