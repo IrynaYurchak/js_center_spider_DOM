6d151392681b541a560c509278c9cbabb445cd95
Iryna <Yurchak.Iryna2017@gmail.com>
2025-10-03 13:55:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function centerSpider() {
+  if (!wall || !spider) {
+    return;
+  }
+
+  const left = (wall.clientWidth - spider.clientWidth) / 2;
+  const height = (wall.clientHeight - spider.clientHeight) / 2;
+
+  spider.style.left = `${left}px`;
+  spider.style.top = `${height}px`;
+}
+
+window.addEventListener('load', () => {
+  if (getComputedStyle(spider).position === 'absolute') {
+    centerSpider();
+  }
+});
